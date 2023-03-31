f4026d092a078d178de7da1a070860c686148cb9
busra <busrahyt42@gmail.com>
2023-04-01 00:29:31 +0300

    ComputerPage has been added.

---
+++ src/test/java/com/amazon/pages/ComputerPage.java
@@ -0,0 +1,39 @@
+package com.amazon.pages;
+
+import com.amazon.utilities.BrowserUtils;
+import com.amazon.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class ComputerPage extends BasePage{
+
+    @FindBy(xpath = "//span[.='\"msi\"']")
+    public WebElement checkOfText;
+
+    @FindBy(linkText = "2")
+    public WebElement secondPage;
+
+    @FindBy(xpath = "//span[@aria-label='Geçerli sayfa, sayfa 2']")
+    public WebElement secondPageAfterMoving;
+
+
+
+
+
+    public WebElement getCheckOfTextWebElement(String text){
+        BrowserUtils.waitForVisibility(By.xpath("//span[.='\""+text+"\"']"),7);
+      return Driver.get().findElement(By.xpath("//span[.='\""+text+"\"']"));
+    }
+
+    public void clickAfterScrollDown(WebElement element){
+        BrowserUtils.scrollToElement(element);
+        BrowserUtils.clickWithJS(element);
+    }
+
+    public WebElement moveToPage(String pageNumber){
+      return Driver.get().findElement(By.xpath("//span[@aria-label='Geçerli sayfa, sayfa "+pageNumber+"']"));
+    }
+
+
+}
