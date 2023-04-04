1016e7506b646b589a26829ddb016d8598f5582d
busra <busrahyt42@gmail.com>
2023-04-05 00:50:52 +0300

    report.html has been updated.

--- src/test/java/com/amazon/tests/AmazonWishlistTest.java
+++ src/test/java/com/amazon/tests/AmazonWishlistTest.java
@@ -6,17 +6,13 @@ import com.amazon.pages.ListPage;
 import com.amazon.pages.LoginPage;
 import com.amazon.utilities.BrowserUtils;
 import com.amazon.utilities.ConfigurationReader;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import java.awt.*;
 
 import static com.amazon.pages.ListPage.currentWishlistSize;
 import static com.amazon.pages.ListPage.priorWishlistSize;
 
-public class AmazonTest extends TestBase {
+public class AmazonWishlistTest extends TestBase {
 
     LoginPage loginPage = new LoginPage();
 
@@ -26,8 +22,8 @@ public class AmazonTest extends TestBase {
 
 
     @Test
-    public void amazonTest() {
-        extentLogger = report.createTest("Shopping List test");
+    public void wishlistTest() {
+        extentLogger = report.createTest("Shopping List Test");
 
         extentLogger.info("go to home page --> " + ConfigurationReader.get("url"));
         driver.get(ConfigurationReader.get("url"));
@@ -53,39 +49,41 @@ public class AmazonTest extends TestBase {
 
         extentLogger.info("create the list whose name is 'SetCard List'");
         dashboardPage.clickCreatingListButton();
-        listPage.createAList("SetCard Liste");
+        listPage.createAList("SetCard List");
 
-        BrowserUtils.waitFor(3);
+        BrowserUtils.waitFor(2);
         extentLogger.info("select the computer category in all categories tab");
         dashboardPage.clickCategoryDropDownBox("Bilgisayarlar");
-        BrowserUtils.waitFor(3);
+
+        BrowserUtils.waitFor(2);
         extentLogger.info("verify that category of computer is selected");
         Assert.assertTrue(dashboardPage.getDropDownMenuWebElement("Bilgisayarlar").isDisplayed());
-        BrowserUtils.waitFor(3);
+
+        BrowserUtils.waitFor(2);
         extentLogger.info("The text of msi is typed into the search box");
         dashboardPage.searchToTextInSearchBox("msi");
 
         extentLogger.info("verify that text of msi is searched");
         Assert.assertTrue(computerPage.getCheckOfTextWebElement("msi").isDisplayed());
-        BrowserUtils.waitFor(3);
+
+        BrowserUtils.waitFor(2);
         extentLogger.info("go to second page on search results page");
         computerPage.clickAfterScrollDown(computerPage.secondPage);
 
         extentLogger.info("verify that navigate to second page on search results page");
-        BrowserUtils.waitForVisibility(computerPage.secondPageAfterMoving, 5);//
+//        BrowserUtils.waitForVisibility(computerPage.secondPageAfterMoving, 5);
+        BrowserUtils.waitFor(3);
         Assert.assertTrue(computerPage.moveToPage("2").isDisplayed());
 
         extentLogger.info("add second product on the page to 'SetCard List'");
-        dashboardPage.addProductToWishList(dashboardPage.secondProduct);
+        dashboardPage.addProductToWishList(dashboardPage.secondProduct,"SetCard List");
 
         extentLogger.info("verify that second product on the page is added to 'SetCard List'");
-        BrowserUtils.waitForVisibility(listPage.textOfAddedProduct, 5);
+        BrowserUtils.waitFor(2);
         Assert.assertTrue(listPage.textOfAddedProduct.isDisplayed());
 
         extentLogger.info("go to Wishlist which is in the myAccount tab ");
-        BrowserUtils.clickWithJS(listPage.continueToShoppingButton);
-        BrowserUtils.scrollToElement(dashboardPage.userAccountName);
-        dashboardPage.clickMyWishlist();
+        listPage.goToWisListAfterAddProductToList();
 
         extentLogger.info("verify that Wishlist page is visible");
         BrowserUtils.waitForPageToLoad(5);
@@ -93,7 +91,11 @@ public class AmazonTest extends TestBase {
 
         extentLogger.info("delete added product in the SetCard List");
         listPage.getDeleteProductFromWishlist();
+
+        extentLogger.info("delete SetCard List");
         listPage.getDeleteWishlist();
+
+        extentLogger.info("verify that deleting process is done.");
         Assert.assertNotEquals(currentWishlistSize, priorWishlistSize);
 
         extentLogger.info("log out from account");
